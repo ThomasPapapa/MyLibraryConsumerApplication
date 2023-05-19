a64270d224fbee6a7ee4376b0e3541a76aaf3f4d
Thomas Papapaschos <tpapapaschos@cognity.gr>
2023-05-19 19:33:45 +0300

    Initial commit

---
+++ app/src/main/java/com/thomas/mylibraryconsumerapplication/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.thomas.mylibraryconsumerapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.thomas.mylibraryconsumerapplication.ui.theme.MyLibraryConsumerApplicationTheme
+import com.thomas.mytestlibraryhelloworld.TestLibraryHelloWorld
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyLibraryConsumerApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(TestLibraryHelloWorld().getHelloWorldText())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyLibraryConsumerApplicationTheme {
+        Greeting("Android")
+    }
+}
